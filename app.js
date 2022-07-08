ee7cb86a0bffde3a268f2dad3737ab62341ad027
Rahul Kailash Yadav <rahuly1903@gmail.com>
2022-07-08 13:13:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cron = require("node-cron");
+const app = express();
+
+const port = 3500;
+
+app.use("/", (req, res) => {
+  res.send({ msg: "hello world" });
+});
+
+// function runEverySecond() {
+//   console.log("running a task every minute");
+// }
+
+const task = cron.schedule("* * * * *", () => {
+  console.log("running a task every minute");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  task.start();
+});
